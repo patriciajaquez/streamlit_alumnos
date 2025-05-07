18c0cb125c1bb68b0b7e7243070140aaa587b11a
Patricia Jaquez <patriciajaquezm@gmail.com>
2025-05-07 16:50:27 +0200

    tabs

--- app.py
+++ app.py
@@ -10,64 +10,6 @@ import plotly.express as px
 
 st.set_page_config(page_title="My First Streamlit App", page_icon=":tada:", layout="wide")
 
-#-------------------------------------------------------------------------------------------
-
-# Texts
-st.title("My First Streamlit App") # Principal title
-
-# Headers
-st.header("This is a header") # Header
-st.subheader("This is a subheader") # Subheader
-
-# Normal text
-st.text("Hello, World!") # Normal text
-
-# Markdown
-st.markdown("## This is a markdown header") # Markdown header
-st.markdown("This is a **markdown** subheader") # Markdown subheader
-
-# Latex
-st.latex(r"""a^2 + b^2 = c^2""") # Latex text
-
-# Code
-st.code("print('Hello, World!')", language="python") # Code text
-
-# Information, warning, error and success messages
-st.info("This is an info message") # Info message
-st.warning("This is a warning message") # Warning message
-st.error("This is an error message") # Error message
-st.success("This is a success message") # Success message
-st.exception("This is an exception message") # Exception message
-
-#-------------------------------------------------------------------------------------------
-
-# Media and resources
-# Images , local route or URL, we can use width= and height= to resize the image
-# st.image("path/to/image.png", caption="Image caption") # Local image
-st.image("https://www.streamlit.io/images/brand/streamlit-mark-color.png", caption="Streamlit logo") # URL image
-
-# Audios
-# Local route or URL, we can use format= to set the audio format
-# st.audio("path/to/audio.mp3") # Local audio
-st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3") # URL audio
-
-# Videos
-# Local route or URL, we can use format= to set the video format
-# st.video("path/to/video.mp4") # Local video
-st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ") # URL video
-
-
-#-------------------------------------------------------------------------------------------
-
-
-df = pd.DataFrame({
-    'Column 1': [1, 2, 3, 8],
-    'Column 2': [4, 5, 6, 15],
-    'Column 3': [7, 8, 9, 22]
-})
-
-st.dataframe(df) # Dataframe
-
 
 #-------------------------------------------------------------------------------------------
 
@@ -78,3 +20,65 @@ st.sidebar.title("Sidebar")
 st.sidebar.header("This is a sidebar header") # Sidebar header
 st.sidebar.subheader("This is a sidebar subheader") # Sidebar subheader
 st.sidebar.text("Hello, World!") # Sidebar normal text
+
+
+#--------------------------------------------------------------------------------------------
+
+tab1, tab2, tab3 = st.tabs(["Text", "Media & Resources", "Dataframe"]) # Tabs
+
+with tab1:
+    # Texts
+    st.title("My First Streamlit App") # Principal title
+
+    # Headers
+    st.header("This is a header") # Header
+    st.subheader("This is a subheader") # Subheader
+
+    # Normal text
+    st.text("Hello, World!") # Normal text
+
+    # Markdown
+    st.markdown("## This is a markdown header") # Markdown header
+    st.markdown("This is a **markdown** subheader") # Markdown subheader
+
+    # Latex
+    st.latex(r"""a^2 + b^2 = c^2""") # Latex text
+
+    # Code
+    st.code("print('Hello, World!')", language="python") # Code text
+
+    # Information, warning, error and success messages
+    st.info("This is an info message") # Info message
+    st.warning("This is a warning message") # Warning message
+    st.error("This is an error message") # Error message
+    st.success("This is a success message") # Success message
+    st.exception("This is an exception message") # Exception message
+
+
+with tab2:
+    # Media and resources
+    # Images , local route or URL, we can use width= and height= to resize the image
+    # st.image("path/to/image.png", caption="Image caption") # Local image
+    st.image("https://www.streamlit.io/images/brand/streamlit-mark-color.png", caption="Streamlit logo") # URL image
+
+    # Audios
+    # Local route or URL, we can use format= to set the audio format
+    # st.audio("path/to/audio.mp3") # Local audio
+    st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3") # URL audio
+
+    # Videos
+    # Local route or URL, we can use format= to set the video format
+    # st.video("path/to/video.mp4") # Local video
+    st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ") # URL video
+
+with tab3:
+    # Dataframes
+    df = pd.DataFrame({
+        'Column 1': [1, 2, 3, 8],
+        'Column 2': [4, 5, 6, 15],
+        'Column 3': [7, 8, 9, 22]
+    })
+
+    st.dataframe(df) 
+
+
